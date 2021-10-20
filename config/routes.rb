c1206714fee21562eea1741ac90e0cb138ecb60b
inouekimi <kool.wxyz@gmail.com>
2021-10-21 05:24:42 +0900

    コンフリクトの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'prices/index'
+  root to: "prices#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :prices, only: :index
 end
