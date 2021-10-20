6d28c8c7c0409f7c696a04c1907a2086ae540767
inouekimi <kool.wxyz@gmail.com>
2021-10-21 07:06:59 +0900

    ActiveHashの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :fullname, :postal_code, :prefecture_code, :city, :street, presence: true
+  validates :nickname, :fullname, :postal_code, :city, :street, presence: true
+  validates :prefecture_code_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
 end
