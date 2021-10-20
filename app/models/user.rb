8f533144ce19d66cf73b114556271a802eadb8b5
inouekimi <kool.wxyz@gmail.com>
2021-10-21 04:35:37 +0900

    devise:viewsの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :fullname, :postal_code, :prefecture_code, :city, :street, presence: true
 end
