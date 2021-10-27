ffc6f1ffab7f5da7919fca30c6d2e830af839def
inouekimi <kool.wxyz@gmail.com>
2021-10-27 18:53:21 +0900

    コメント機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, :fullname, :postal_code, :city, :street, presence: true
-  validates :prefecture_code_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  
+  with_options presence: true do 
+    validates :fullname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' }
+    validates :nickname, format: { with: /\A[a-z0-9]+\z/,message: '半角英数字を使用してください' }
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :city, :street
+    validates :prefecture_code_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'prices/index'
   root to: "prices#index"
   devise_for :users
+  resources :comments, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :prices, only: :index
 end
