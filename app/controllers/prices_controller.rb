162e2c7d4f47f715bc8ca81184a145d7e7b96702
inouekimi <kool.wxyz@gmail.com>
2021-10-31 14:19:40 +0900

    Active Storageの実装

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,25 +1,20 @@
 class PricesController < ApplicationController
   def index
-    @comments = Price.all
+    @comments = Price.all.order("created_at DESC")
     @comment = Price.new
   end
   
   def create
     @comment = Price.new(comment_params)
     if @comment.save
-      # もし入力したデータがsaveできたら
       redirect_to root_path
-      # 更新してトップページへ戻る
     else
-      # saveできなかったら
       render :index
-      # ビューのitem/newを表示する
     end
   end
 
   private
   def comment_params
-    params.permit(:text).merge(user_id: current_user.id)
+    params.permit(:text, :image).merge(user_id: current_user.id)
   end
-
 end
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,9 @@
 class Price < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :text, presence: true, unless: :was_attached?
+  def was_attached?
+    self.image.attached?
+  end
 end
